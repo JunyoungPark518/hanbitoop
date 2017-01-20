411b23bf9660e7ad2b8952eff24763b494a5d52f
JunyoungPark518 <babungv@gmail.com>
2017-01-20 17:22:51 +0900

    Using Swing upload2

--- src/bank/Controller.java
+++ src/bank/Controller.java
@@ -10,10 +10,10 @@ public class Controller {
 		Account account = null;
 		String temp = "";
 		while(true) {
-			switch(inputInt("0.종료 / 1.회원등록 / 2.통장개설 / 3.입금 / 4.출금 / 5.조회 / 6.해지")) {
+			switch(inputInt("1.회원등록\n2.통장개설\n3.입금\n4.출금\n5.조회\n6.해지\n0.종료")) {
 			case 0:
 				return;
-			case 1:
+			case 1: // 회원등록
 				member = new Member();
 				member.setName(input("이름을 입력하세요."));
 				while(true) {
@@ -29,7 +29,7 @@ public class Controller {
 				member.setUid(input("아이디를 입력하세요."));
 				show(member.getName() + "님 회원등록이 완료되었습니다.");
 				break;
-			case 2:
+			case 2: // 통장개설
 				String accountType = "저축예금";
 				while(true) {
 					if(input("아이디를 입력하세요.").equals(member.getUid())) {
@@ -48,18 +48,49 @@ public class Controller {
 					}
 				}
 				break;
-			case 3:
+			case 3: // 입금
+				if(input("아이디를 입력하세요.").equals(member.getUid()) && account != null) {
+					account.setMoney(account.getMoney() + inputInt("얼마를 입금하시겠습니까?"));
+					show(String.format("입금이 완료되었습니다.\n현재 잔액은 %d원입니다.", account.getMoney()));
+				} else {
+					notCase(account);
+				}
 				break;
-			case 4:
+			case 4: // 출금
+				if(input("아이디를 입력하세요.").equals(member.getUid()) && account != null) {
+					int withdraw = inputInt("얼마를 출금하시겠습니까?");
+					if(account.getMoney() - withdraw >= 0) {
+						account.setMoney(account.getMoney() - withdraw);
+						show(String.format("출금이 완료되었습니다.\n현재 잔액은 %d원입니다.", account.getMoney()));
+					} else {
+						show("잔액이 부족하여 출금할 수 없습니다.");
+					}
+				} else {
+					notCase(account);
+				}
 				break;
-			case 5:
+			case 5: // 조회
+				if(input("아이디를 입력하세요.").equals(member.getUid()) && account != null) {
+					show(String.format("%s님의 현재 잔액은 %d원입니다.", member.getName(), account.getMoney()));
+				} else {
+					notCase(account);
+				}
 				break;
-			case 6:
+			case 6: // 해지
+				if(input("아이디를 입력하세요.").equals(member.getUid()) && account != null) {
+					account = (input("정말 해지하실 건가요?\n해지하시려면 '해지'를 입력하세요.").equals("해지")) ? null : account;
+					if(account==null) {
+						show("해지가 완료되었습니다.");
+					}
+				} else {
+					notCase(account);
+				}
 				break;
 			}
 		}
 	}
 	
+	// 사용 편의를 위한 추가 생성 메서드
 	private String input(String str) {
 		return JOptionPane.showInputDialog(str);
 	}
@@ -70,5 +101,13 @@ public class Controller {
 	
 	private int inputInt(String str) {
 		return Integer.parseInt(JOptionPane.showInputDialog(str));
+	}
+	
+	private void notCase(Account a) {
+		if(a == null) {
+			show("통장을 먼저 개설하세요.");
+		} else {
+			show("아이디를 잘못 입력하셨습니다.");
+		}
 	}
 }
--- src/test/Controller.java
+++ src/test/Controller.java
@@ -1,61 +1,49 @@
 package test;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Controller {
-	public static void main(String[] args) { // local variable 반드시 initialize 한다.
-		Controller c = new Controller();
-		c.start();
-	}
-	
 	public void start() {
-		Scanner sc = new Scanner(System.in);
 		String ssn = "", gender = "", id = "";
 		Member member = null; // class object의 초기값은 null
 		BMI bmi = null;
 		while(true) {
-			System.out.println("1. 회원등록 / 2. BMI측정 / 3. 로또 / 4. 성적표 / 0. 종료");
-			int choice = sc.nextInt();
-			switch(choice) {
+			switch(inputInt("1. 회원등록 / 2. BMI측정 / 3. 로또 / 4. 성적표 / 0. 종료")) {
 			case 0:
 				return;
 			case 1:
 				member = new Member();
 				System.out.println("아이디,이름,주민번호앞자리(800101-1)");
 				// input ID
-				member.setUid(sc.next());
+				member.setUid(input("아이디"));
 				// input Name
-				member.setName(sc.next());
+				member.setName(input("이름"));
 				// input SSN
-				ssn = sc.next();
+				ssn = input("주민번호앞자리(800101-1)");
 				while(true) {
 					char ch = ssn.charAt(7);
 					if(ch == '0' || ch == '7' || ch == '8' || ch == '9') {
-						System.out.println("다시 입력하세요.");
-						ssn = sc.next();
+						ssn = input("다시 입력하세요.");
 					} else {
 						break;
 					}
 				}
 				member.setSsn(ssn);
 				gender = member.calcGender(ssn);
-				System.out.printf("%s은 %s입니다.\n", member.getName(), gender);	
-				System.out.println("회원등록 되었습니다.");
+				show(String.format("%s은 %s입니다.\n회원등록 되었습니다.", member.getName(), gender));
 				break;
 			case 2:
 				bmi = new BMI();
 				while(true) {
-					System.out.println("아이디?");
-					id = sc.next();
+					id = input("아이디?");
 					if(id.equals(member.getUid())) {
 						bmi.setUid(id);
-						System.out.println("몸무게,키?");
-						bmi.setHeight(sc.nextDouble() / 100);
-						bmi.setWeight(sc.nextDouble());
-						System.out.printf("%s은 '%s'입니다.\n", member.getName(), bmi.calcBmi(bmi.getHeight(), bmi.getWeight()));
+						bmi.setHeight(inputDouble("몸무게?") / 100);
+						bmi.setWeight(inputDouble("키?"));
+						show(String.format("%s은 '%s'입니다.\n", member.getName(), bmi.calcBmi(bmi.getHeight(), bmi.getWeight())));
 						break;
 					} else {
-						System.out.println("아이디를 잘못 입력하셨습니다.");
+						show("아이디를 잘못 입력하셨습니다.");
 					}
 				}
 				break;
@@ -65,12 +53,9 @@ public class Controller {
 				break;
 			case 4:
 				ReportCard r = new ReportCard();
-				System.out.print("Name:");
-				r.setName(sc.next());
+				r.setName(input("이름"));
 				while(r.getSubjects()<6) {
-					System.out.println("If you want to stop, Type -1");
-					System.out.print("Score:");
-					r.setScore(sc.nextInt());
+					r.setScore(inputInt("If you want to stop, Type -1\nScore?"));
 					if(r.getScore() == -1) {
 						break;
 					}
@@ -78,15 +63,31 @@ public class Controller {
 					r.setSubjects(r.getSubjects() + 1);
 				}
 				r.calc();
-				System.out.printf("Name: %s\nSubjects: %s\nTotal: %s\nAverage: %s\nGrade: %s",
-						r.getName(),r.getSubjects(),r.getTotal(),r.getAverage(),r.getGrade());
+				show(String.format("Name: %s\nSubjects: %s\nTotal: %s\nAverage: %s\nGrade: %s",
+						r.getName(),r.getSubjects(),r.getTotal(),r.getAverage(),r.getGrade()));
 				break;
 			case 5:
 				break;
 			default:
-				System.out.println("프로그램이 종료됩니다.");
+				show("프로그램이 종료됩니다.");
 				return;
 			}
 		}
+	}
+	
+	private String input(String str) {
+		return JOptionPane.showInputDialog(str);
+	}
+	
+	private void show(String str) {
+		JOptionPane.showMessageDialog(null, str);
+	}
+	
+	private int inputInt(String str) {
+		return Integer.parseInt(JOptionPane.showInputDialog(str));
+	}
+	
+	private double inputDouble(String str) {
+		return Double.parseDouble(JOptionPane.showInputDialog(str));
 	}
 }
