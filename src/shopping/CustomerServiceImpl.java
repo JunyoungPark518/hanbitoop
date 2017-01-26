8d85ca90eb74397d8e53e8a3323ebe407ffddbd4
JunyoungPark518 <babungv@gmail.com>
2017-01-26 17:41:33 +0900

    commit

--- src/shopping/CustomerServiceImpl.java
+++ src/shopping/CustomerServiceImpl.java
@@ -3,15 +3,16 @@ package shopping;
 public class CustomerServiceImpl implements CustomerService {
 	Customer customer;
 	int index;
+	Product[] cart; // 다형성
 	public CustomerServiceImpl() {
 		customer = new Customer();
 		index = 0;
+		cart = new Product[4];
 	}
-	
+	/*  물건 구매시 현재 금액과 차이를 보고 구매 가능한지를 따진다.
+		만약 가진 돈이 작으면 잔액이 부족합니다 라고 한다. */
 	@Override
 	public String buy(Product p) {
-		// 물건 구매시 현재 금액과 차이를 보고 구매 가능한지를 따진다.
-		// 만약 가진 돈이 작으면 잔액이 부족합니다 라고 한다
 		String pname = p.getName();
 		String result = "잔액이 부족함\n남은 잔액:" + customer.getMoney();
 		if(customer.getMoney() >= p.getPrice()) {
@@ -22,21 +23,21 @@ public class CustomerServiceImpl implements CustomerService {
 		return result;
 	}
 
+	/* 카트에 추가시키는 로직 */
 	@Override
 	public void add(Product p) {
-		// 카트에 추가시키는 로직
-		customer.getCart()[index] = p;
+		cart[index] = p;
 		index++;
 	}
 
+	/* 쇼핑을 마치고 영수증을 출력한다.
+	   TV ... 300
+	   지금 상황에선 3개만 구입한 내용이 나온다 */
 	@Override
 	public String getBill() {
-		// 쇼핑을 마치고 영수증을 출력한다.
-		// TV ... 300
-		// 지금 상황에선 3개만 구입한 내용이 나온다
 		String bill = "";
 		for(int i=0; i<index; i++) {
-			bill += customer.getCart()[i].toString() + "\n";
+			bill += cart[i].toString() + "\n";
 		}
 		return bill;
 	}
